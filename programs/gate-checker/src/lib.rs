464c805f9c3b53368c9e9442ef47b5d8e63d126e
Eloi <es@gsmk.de>
2024-11-27 10:09:03 +0100

    minting token working

--- programs/gate-checker/src/lib.rs
+++ programs/gate-checker/src/lib.rs
@@ -2,116 +2,131 @@ use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
     metadata::{
-        create_master_edition_v3, create_metadata_accounts_v3, CreateMasterEditionV3,
-        CreateMetadataAccountsV3, Metadata, MetadataAccount,
-        },
-    token::{mint_to, Mint, MintTo, Token, TokenAccount},
+        create_metadata_accounts_v3,
+        mpl_token_metadata::types::DataV2,
+        CreateMetadataAccountsV3,
+        Metadata as Metaplex,
+    },
+    token::{ mint_to, Mint, MintTo, Token, TokenAccount },
 };
 
-use mpl_token_metadata::{
-    pda::{find_master_edition_account, find_metadata_account},
-    state::DataV2
-};
-
-// declare_id!("DjEiZonFEZpw4M33ZCpMWnrshPwEJ5ZNmuV8FWQiVWdg");
-declare_id!("4bFa2t9VRRj9XtDhrFEDbgvEirREdFCqtYb6pYcjXkki");
+declare_id!("5jetUBVEoZBqxWyrcRBvEhmxHEHeV86fB2HM7AajAJuo");
 
 #[program]
-pub mod gate_checker {
+mod gate_checker {
     use super::*;
+    pub fn init_token(ctx: Context<InitToken>, metadata: InitTokenParams) -> Result<()> {
+        let seeds = &["mint".as_bytes(), &[ctx.bumps.mint]];
+        let signer = [&seeds[..]];
 
-  pub fn init_nft(ctx: Context<InitNFT>, name: String, symbol: String, uri: String) -> Result<()> {
-        // create mint account
-        let cpi_context = CpiContext::new(
-            ctx.accounts.token_program.to_account_info(),
-            MintTo {
-                mint: ctx.accounts.mint.to_account_info(),
-                to: ctx.accounts.associated_token_account.to_account_info(),
-                authority: ctx.accounts.signer.to_account_info(),
-            },
-        );
-
-        mint_to(cpi_context, 1)?;
-
-        let cpi_context = CpiContext::new(
-            ctx.accounts.token_metadata_program.to_account_info(),
-            CreateMetadataAccountsV3 {
-                metadata: ctx.accounts.metadata_account.to_account_info(),
-                mint: ctx.accounts.mint.to_account_info(),
-                mint_authority: ctx.accounts.signer.to_account_info(),
-                update_authority: ctx.accounts.signer.to_account_info(),
-                payer: ctx.accounts.signer.to_account_info(),
-                system_program: ctx.accounts.system_program.to_account_info(),
-                rent: ctx.accounts.rent.to_account_info(),
-            },
-        );
-
-        let data_v2 = DataV2 {
-            name: name,
-            symbol: symbol,
-            uri: uri,
+        let token_data: DataV2 = DataV2 {
+            name: metadata.name,
+            symbol: metadata.symbol,
+            uri: metadata.uri,
             seller_fee_basis_points: 0,
             creators: None,
             collection: None,
             uses: None,
         };
 
-        create_metadata_accounts_v3(cpi_context, data_v2, false, true, None)?;
-
-        let cpi_context = CpiContext::new(
-            ctx.accounts.master_edition_account.to_account_info(),
-            CreateMasterEditionV3 {
-                edition: ctx.accounts.master_edition_account.to_account_info(),
+        //Note: Because we used init in our mint account, we do not need to invoke the create_mint instruction from the SPL Token program. This will be handled automatically by Anchor behind the scenes.
+        let metadata_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_metadata_program.to_account_info(),
+            CreateMetadataAccountsV3 {
+                payer: ctx.accounts.payer.to_account_info(),
+                update_authority: ctx.accounts.mint.to_account_info(),
                 mint: ctx.accounts.mint.to_account_info(),
-                update_authority: ctx.accounts.signer.to_account_info(),
-                mint_authority: ctx.accounts.signer.to_account_info(),
-                payer: ctx.accounts.signer.to_account_info(),
-                metadata: ctx.accounts.metadata_account.to_account_info(),
-                token_program: ctx.accounts.token_program.to_account_info(),
+                metadata: ctx.accounts.metadata.to_account_info(),
+                mint_authority: ctx.accounts.mint.to_account_info(),
                 system_program: ctx.accounts.system_program.to_account_info(),
                 rent: ctx.accounts.rent.to_account_info(),
-            }
+            },
+            &signer
         );
 
+        create_metadata_accounts_v3(metadata_ctx, token_data, false, true, None)?;
 
-        create_master_edition_v3(cpi_context, None)?;
+        msg!("Token mint created successfully.");
+
+        Ok(())
+    }
+
+    pub fn mint_tokens(
+        ctx: Context<MintTokens>,
+        quantity: u64
+    ) -> Result<()> {
+        let seeds = &["mint".as_bytes(), &[ctx.bumps.mint]];
+        let signer = [&seeds[..]];
+
+            // Mint the NFT to the wallet's associated token account
+            mint_to(
+                CpiContext::new_with_signer(
+                    ctx.accounts.token_program.to_account_info(),
+                    MintTo {
+                        authority: ctx.accounts.mint.to_account_info(),
+                        to: ctx.accounts.destination_ata.to_account_info(),
+                        mint: ctx.accounts.mint.to_account_info(),
+                    },
+                    &signer
+                ),
+                quantity
+            )?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct InitNFT<'info> {
-    #[account(mut, signer)]
-    /// CHECK: <comment explaining why are we blindly trusting this account>.
-    pub signer: AccountInfo<'info>,
+#[instruction(params: InitTokenParams)]
+pub struct InitToken<'info> {
+    /// CHECK: New Metaplex Account being created
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
     #[account(
         init,
-        payer = signer,
-        mint::decimals = 0,
-        mint::authority = signer.key(),
-        mint::freeze_authority = signer.key(),
+        seeds = [b"mint"],
+        bump,
+        payer = payer,
+        mint::decimals = params.decimals,
+        mint::authority = mint //You'll notice that we set the authority to the mint account itself. This effectively gives authority to our program without having to create another PDA.
     )]
     pub mint: Account<'info, Mint>,
-      #[account(
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub token_metadata_program: Program<'info, Metaplex>,
+}
+
+#[derive(Accounts)]
+pub struct MintTokens<'info> {
+    #[account(
+        mut,
+        seeds = [b"mint"],
+        bump,
+        mint::authority = mint
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    /// CHECK: Associated Token Account to be created.
+    #[account(
         init_if_needed,
-        payer = signer,
+        payer = payer,
         associated_token::mint = mint,
-        associated_token::authority = signer,
+        associated_token::authority = payer
     )]
-    pub associated_token_account: Account<'info, TokenAccount>,
-    #[account(
-        mut,
-        address=find_metadata_account(&mint.key()).0,
-    )]
-    pub metadata_account: AccountInfo<'info>,
-    #[account(
-        mut,
-        address=find_master_edition_account(&mint.key()).0,
-    )]
-    pub master_edition_account: AccountInfo<'info>,
+    pub destination_ata: Account<'info, TokenAccount>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
-    pub token_metadata_program: Program<'info, Metadata>,
-    pub system_program: Program<'info, System>,
-    pub rent: Sysvar<'info, Rent>
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Debug, Clone)]
+pub struct InitTokenParams {
+    pub name: String,
+    pub symbol: String,
+    pub uri: String,
+    pub decimals: u8,
 }
--- tests/gate-checker.test.ts
+++ tests/gate-checker.test.ts
@@ -1,59 +1,130 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { PublicKey, SystemProgram } from "@solana/web3.js";
-import { assert } from "chai";
+import * as anchor from '@coral-xyz/anchor';
+import { Program, BN } from '@coral-xyz/anchor';
+import {
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  SYSVAR_RENT_PUBKEY,
+  Transaction
+} from '@solana/web3.js';
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  MINT_SIZE,
+  TOKEN_PROGRAM_ID,
+  createAssociatedTokenAccountIdempotentInstruction,
+  createInitializeMint2Instruction,
+  createMintToInstruction,
+  getAssociatedTokenAddressSync,
+  getMinimumBalanceForRentExemptMint
+} from '@solana/spl-token';
 
-describe("gate_checker", () => {
-  // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.local();
-  anchor.setProvider(provider);
+import { GateChecker } from '../target/types/gate_checker';
+import { token } from '@coral-xyz/anchor/dist/cjs/utils';
+import { assert } from 'chai';
 
-  const program = anchor.workspace.GateChecker as Program;
+describe('NFT_mint', () => {
+  anchor.setProvider(anchor.AnchorProvider.env());
 
-  it("Initializes an NFT", async () => {
-    // Generate a new keypair for the signer
-    const signer = anchor.web3.Keypair.generate();
+  const provider = anchor.getProvider();
 
-    // Airdrop SOL to the signer to pay for transactions
-    const signature = await provider.connection.requestAirdrop(
-      signer.publicKey,
-      1_000_000_000 // 1 SOL
+  const connection = provider.connection;
+
+  const program = anchor.workspace.GateChecker as Program<GateChecker>;
+
+  const METADATA_SEED = 'metadata'; // Seed for the metadata account
+  const TOKEN_METADATA_PROGRAM_ID = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s');
+  const MINT_SEED = 'mint';
+  const payer = program.provider.publicKey;
+
+  const metadata = {
+    name: 'NuKIO token',
+    symbol: 'NUKIO',
+    uri: 'https://5vfxc4tr6xoy23qefqbj4qx2adzkzapneebanhcalf7myvn5gzja.arweave.net/7UtxcnH13Y1uBCwCnkL6APKsge0hAgacQFl-zFW9NlI',
+    decimals: 9
+  }; // Metadata for the NFT
+
+  const mintAmount = 5;
+
+  const [mint] = PublicKey.findProgramAddressSync(
+    [Buffer.from(MINT_SEED, 'utf-8')], // Seed for the mint account
+    program.programId
+  );
+
+  const [metadataAddress] = PublicKey.findProgramAddressSync(
+    [Buffer.from(METADATA_SEED, 'utf-8'), TOKEN_METADATA_PROGRAM_ID.toBuffer(), mint.toBuffer()],
+    TOKEN_METADATA_PROGRAM_ID
+  );
+
+  const log = async (signature: string): Promise<string> => {
+    console.log(
+      `Your transaction signature: https://explorer.solana.com/transaction/${signature}?cluster=custom&customUrl=${connection.rpcEndpoint}`
     );
-    await provider.connection.confirmTransaction(signature, "confirmed");
+    return signature;
+  };
 
-    // Define PDA and accounts
-    const mint = anchor.web3.Keypair.generate();
-    const associatedTokenAddress = await anchor.utils.token.associatedAddress({
-      mint: mint.publicKey,
-      owner: signer.publicKey,
+  it('Initialize Token', async () => {
+    const info = await program.provider.connection.getAccountInfo(mint);
+    if (info) {
+      return;
+    }
+    console.log('Mint not found. Initializing...');
+
+    const context = {
+      metadata: metadataAddress,
+      mint,
+      payer,
+      rent: SYSVAR_RENT_PUBKEY,
+      systemProgram: SystemProgram.programId,
+      tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+      tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID
+    };
+
+    const txHash = await program.methods.initToken(metadata).accounts(context).rpc();
+
+    await program.provider.connection.confirmTransaction(txHash); // Confirm the transaction
+    log(txHash);
+    const newInfo = await program.provider.connection.getAccountInfo(mint);
+    assert(newInfo, 'Mint should be initialized');
+  });
+
+  it('Mint token', async () => {
+    const destination = await anchor.utils.token.associatedAddress({
+      owner: payer,
+      mint: mint
     });
 
-    // Call the init_nft instruction
-    await program.methods
-      .initNft()
-      .accounts({
-        signer: signer.publicKey,
-        mint: mint.publicKey,
-        associatedTokenAccount: associatedTokenAddress,
-        tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
-        associatedTokenProgram: anchor.utils.token.ASSOCIATED_PROGRAM_ID,
-        systemProgram: SystemProgram.programId,
-        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-      })
-      .signers([signer, mint])
-      .rpc();
+    let initialBalance: number;
 
-    // Fetch the associated token account to verify it was created
-    const tokenAccount = await provider.connection.getAccountInfo(
-      associatedTokenAddress
-    );
+    try {
+      const balance = await program.provider.connection.getTokenAccountBalance(destination);
+      initialBalance = balance.value.uiAmount;
+    } catch (error) {
+      //Token account not yet initialized, then has 0 balance
+      initialBalance = 0;
+    }
 
-    // Assertions
-    assert.ok(tokenAccount !== null, "Associated Token Account was not created");
-    assert.equal(
-      tokenAccount.owner.toBase58(),
-      anchor.utils.token.TOKEN_PROGRAM_ID.toBase58(),
-      "Incorrect owner for associated token account"
-    );
+    const context = {
+      mint,
+      destinationAta: destination,
+      payer,
+      rent: SYSVAR_RENT_PUBKEY,
+      systemProgram: SystemProgram.programId,
+      tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
+      associatedTokenProgram: anchor.utils.token.ASSOCIATED_PROGRAM_ID
+    };
+
+    const txHash = await program.methods.mintTokens(new BN(mintAmount * 10 ** metadata.decimals)).accounts(context).rpc(); // Mint 5 tokens
+    await program.provider.connection.confirmTransaction(txHash); // Confirm the transaction
+    log(txHash);
+
+    // const postBalance = (await program.provider.connection.getTokenAccountBalance(destination))
+    //   .value.uiAmount;
+
+    // assert.equal(
+    //   initialBalance + mintAmount,
+    //   postBalance,
+    //   'Post balance should be equal initial plus mint aamount'
+    // );
   });
 });
